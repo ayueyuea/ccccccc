f550d727d9e0713cef3ed4f37c373c2427965884
胡学悦 <1245124561@qq.com>
2022-07-06 17:07:53 +0800

    12

--- fs模块实际应用.js
+++ fs模块实际应用.js
@@ -6,7 +6,7 @@ let url = path.join(__dirname, 'text.html')
 // console.log('文件名          -->', path.basename(url));                      // 文件名          --> 任务报告书.txt
 // console.log('文件名(不带后缀)-->', path.basename(url, path.extname(url)));   // 文件名(不带后缀)--> 任务报告书
 // console.log('文件后缀        -->', path.extname(url));                       // 文件后缀        --> .txt
-// console.log('文件路径        -->', path.dirname(url));                       // 文件路径        --> E:\学习demo\NodeJs学习
+// console.log('文件路径        -->', path.dirname(url));                       // 文件路径        --> E:\学习demo\NodeJs学习 
 
 new Promise((resolve, reject) => {
     fs.readFile(url, 'utf-8', (err, data) => {
